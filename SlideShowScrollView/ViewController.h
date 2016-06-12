ba85f43f1dba125b479bd1a9e6f31b9e7e277f6d
buderui <buderui@buderuidemac-mini.local>
2016-06-12 09:58:17 +0800

    Initial Commit

---
+++ SlideShowScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideShowScrollView
+//
+//  Created by buderui on 16/6/12.
+//  Copyright © 2016年 buderui HuHeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideShowScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideShowScrollView
+//
+//  Created by buderui on 16/6/12.
+//  Copyright © 2016年 buderui HuHeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
